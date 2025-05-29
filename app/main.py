81418c85cecefd65c4e369eab4f5faffd9096417
Lennart Bob <lennarttimmermans@gmail.com>
2025-05-29 13:28:20 +0200

    a

--- app/main.py
+++ app/main.py
@@ -30,19 +30,6 @@ app.add_middleware(
 # Add the routers
 app.include_router(con_summ.router)
 
-def register_exception(app: FastAPI):
-  @app.exception_handler(RequestValidationError)
-  async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
-    for err in exc.errors():
-      pprint(err)
-    content = {"status_code": 10422, "message": exc_str, "data": None}
-    return JSONResponse(
-      content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
-    )
-
-register_exception(app)
-
 @app.get(path="/", response_model=dict[str, str])
 async def root() -> dict[str, str]:
   logger.info("Welcome to the Graph Foundry App")
---
+++ testing/endpoint.py
@@ -0,0 +1,6 @@
+import requests
+
+url = "http://127.0.0.1:8000/summerize"
+
+
+r = requests.post(url, data)
