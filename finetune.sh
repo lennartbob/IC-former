44d68396cdbeab16067cf8e4fe9f8444fe2f17ac
Lennart Bob <lennarttimmermans@gmail.com>
2025-05-29 10:28:37 +0200

    a

--- finetune.sh
+++ finetune.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=0 python finetune.py \
 --base_lm_path meta-llama/Meta-Llama-3.1-8B-Instruct \
 --save_path ./output \
 --max_epoch 3 \
---max_seq_len 1024 \
+--max_seq_len 768  \
 --max_chunk_len 512 \
 --save_interval 5000 \
 --gradient_accumulation 128 \
